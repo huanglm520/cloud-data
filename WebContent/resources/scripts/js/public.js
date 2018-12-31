3904fe50d3fb956b112208a799f2789fa9eb8b37
huanglm <2788447660@qq.com>
2018-12-31 17:40:02 +0800

    删除项目内静态资源，删除静态资源映射

--- WebContent/resources/scripts/js/public.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("#logout").click(function () {
-		$.post(GetPath() + "/passport/logout/", function() {
-			window.location.href = GetPath() + "/passport/login"
-		});
-	});
-});
